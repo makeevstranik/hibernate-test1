ce6acef8a3986861f37b9754942e9bb5654d8014
makeev <makeevstranik@gmail.com>
2022-06-04 03:02:22 +0500

    done, db password deleted

---
+++ hibernate-jvision/src/main/java/com/makeev/model/Engine.java
@@ -0,0 +1,15 @@
+package com.makeev.model;
+
+import lombok.*;
+
+/**
+ * Author : Makeev Evgeny
+ * Created 03/06/22
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Engine {
+    private String model;
+    private int power;
+}
